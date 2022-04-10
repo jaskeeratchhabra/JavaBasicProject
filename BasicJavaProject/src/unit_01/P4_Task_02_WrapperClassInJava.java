fcc1dcf62366ea3e9b5dff2477c75a49880e3d1d
jaskeeratchhabra <jkchhabra99@gmail.com>
2022-04-10 19:59:32 +0530

    modifies

--- BasicJavaProject/src/unit_01/P4_Task_02_WrapperClassInJava.java
+++ BasicJavaProject/src/unit_01/P4_Task_02_WrapperClassInJava.java
@@ -4,7 +4,23 @@ public class P4_Task_02_WrapperClassInJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 20;
+		String s = "" + a + "";
+		
+		Integer i = Integer.valueOf(a);
+		Integer j = a;
+		
+		System.out.println(a + " " + i + " " + j);
+		System.out.println(i.toString());
+		
+		byte b=10;
+		Byte byteobj = b;
+		
+		System.out.println(byteobj);
+		
+		byte bytevalue = byteobj;
+		System.out.println(bytevalue);
+		
 
 	}
-
 }
--- BasicJavaProject/src/unit_01/P5_Task_02_MethodsInJava.java
+++ BasicJavaProject/src/unit_01/P5_Task_02_MethodsInJava.java
@@ -3,8 +3,5 @@ package unit_01;
 public class P5_Task_02_MethodsInJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		
 }
