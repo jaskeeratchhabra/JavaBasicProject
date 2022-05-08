48c54480b461be491a4d7e1fe7a601b95a59213e
jaskeeratchhabra <jkchhabra99@gmail.com>
2022-05-08 15:50:03 +0530

    new push

--- BasicJavaProject/src/Assignment_01/q2.java
+++ BasicJavaProject/src/Assignment_01/q2.java
@@ -1,11 +1,25 @@
+
 package Assignment_01;
-
-public class q2 {
-
+public class q2
+{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		
+        ABC obj1=new ABC();
+        ABC obj2=new ABC();
+        ABC obj3=new ABC();
+        ABC obj4=new ABC();
+        ABC.display();
 	}
-
+}
+class ABC
+{
+    static int count=0;
+    ABC()
+    {
+        count++;
+    }
+    static void  display()
+    {
+        System.out.println(count);
+    }
 }
